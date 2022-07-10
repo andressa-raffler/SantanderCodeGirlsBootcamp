3c9de8deae8cdc4af47c45135e7896324339be64
Andressa Raffler <andressa.raffler@gmail.com>
2022-07-10 20:27:57 -0300

    Initial commit

---
+++ src/main/java/web/me/dio/bootcamp/santander/codegirls/App.java
@@ -0,0 +1,14 @@
+package web.me.dio.bootcamp.santander.codegirls;
+
+/**
+ * Bootcamp da Dio
+ * Santander Develas
+ * Julho - 2022
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
